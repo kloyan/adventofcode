4b938e7f0743290e3408ef9f6d6d6d76b34f9593
Kaloyan <kaloyan.simitchiyski@gmail.com>
2022-12-18 21:55:35 +0200

    Add solution for day 16 part 2

--- day16/main.go
+++ day16/main.go
@@ -12,75 +12,62 @@ import (
 //go:embed input.txt
 var input string
 
-type valve struct {
-	idx       int
-	neighbors []int
-	rate      int
-}
-
 type key struct {
-	idx  int
-	time int
-	mask int
+	idx          int
+	time         int
+	mask         int
+	withElephant bool
 }
 
 func main() {
 	re := regexp.MustCompile(`Valve ([A-Z]+) has flow rate=([0-9]+); tunnels? leads? to valves? ((?:[A-Z]+,?)+)`)
-	indices := map[string]int{}
-	split := strings.Split(strings.TrimSpace(input), "\n")
+	valves := map[string]int{}
+	tunnels := map[string][]string{}
 
-	start := 0
-	for i, str := range split {
-		var v string
-		fmt.Sscanf(str, "Valve %s has flow rate", &v)
-		indices[v] = i
-
-		if v == "AA" {
-			start = i
-		}
+	for _, str := range strings.Split(strings.TrimSpace(input), "\n") {
+		m := re.FindStringSubmatch(strings.ReplaceAll(str, ", ", ","))
+		valves[m[1]] = common.ToInt(m[2])
+		tunnels[m[1]] = strings.Split(m[3], ",")
 	}
 
-	valves := []valve{}
-	for _, str := range split {
-		m := re.FindStringSubmatch(strings.ReplaceAll(str, ", ", ","))
-		valve := valve{
-			idx:  indices[m[1]],
-			rate: common.ToInt(m[2]),
-		}
-		for _, n := range strings.Split(m[3], ",") {
-			valve.neighbors = append(valve.neighbors, indices[n])
-		}
-		valves = append(valves, valve)
+	indices := map[string]int{}
+	for v := range valves {
+		indices[v] = len(indices)
 	}
 
 	cache := map[key]int{}
-	ans := dfs(valves[start], 30, 0, cache, valves)
+	part1 := dfs("AA", 30, 0, false, cache, indices, valves, tunnels)
+	fmt.Println("Answer for Part 1:", part1)
 
-	fmt.Println("Answer for Part 1:", ans)
+	part2 := dfs("AA", 26, 0, true, cache, indices, valves, tunnels)
+	fmt.Println("Answer for Part 2:", part2)
 }
 
-func dfs(curr valve, time int, mask int, cache map[key]int, valves []valve) int {
-	if time < 1 {
+func dfs(curr string, time int, mask int, withElephant bool, cache map[key]int, indices map[string]int, valves map[string]int, tunnels map[string][]string) int {
+	if time == 0 {
+		if withElephant {
+			return dfs("AA", 26, mask, false, cache, indices, valves, tunnels)
+		}
 		return 0
 	}
 
-	key := key{idx: curr.idx, time: time, mask: mask}
-	if m, ok := cache[key]; ok {
-		return m
+	key := key{idx: indices[curr], time: time, mask: mask, withElephant: withElephant}
+	if c, ok := cache[key]; ok {
+		return c
 	}
 
-	ans := 0
-	bit := 1 << curr.idx
-	if (mask&bit == 0) && curr.rate > 0 {
-		ans = (time-1)*curr.rate + dfs(curr, time-1, mask|bit, cache, valves)
+	val := 0
+	bit := 1 << indices[curr]
+	if mask&bit == 0 && valves[curr] > 0 {
+		val = (time-1)*valves[curr] + dfs(curr, time-1, mask|bit, withElephant, cache, indices, valves, tunnels)
 	}
 
-	for _, n := range curr.neighbors {
-		ans = max(ans, dfs(valves[n], time-1, mask, cache, valves))
+	for _, n := range tunnels[curr] {
+		val = max(val, dfs(n, time-1, mask, withElephant, cache, indices, valves, tunnels))
 	}
 
-	cache[key] = ans
-	return ans
+	cache[key] = val
+	return val
 }
 
 func max(a, b int) int {
